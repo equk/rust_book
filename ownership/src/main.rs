448d5a223b70c1e2a5ed9f1cdd0aabc4f39d9519
equk <equk@users.noreply.github.com>
2018-08-24 19:10:00 +0000

    added ownership code - chapter 4

---
+++ ownership/src/main.rs
@@ -0,0 +1,8 @@
+#![allow(unused_variables)]
+fn main() {
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // This will print `hello, world!`
+}
